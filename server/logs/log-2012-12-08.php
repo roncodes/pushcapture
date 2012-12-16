1436ac4e458480115368821b83d66b75f50b4c61
Ronald A. Richardson <theprestig3@gmail.com>
2012-12-15 18:20:01 -0700

    cleaned

--- server/logs/log-2012-12-08.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2012-12-08 17:23:51 --> Severity: Notice  --> mysql_pconnect() [<a href='function.mysql-pconnect'>function.mysql-pconnect</a>]: send of 5 bytes failed with errno=10053 An established connection was aborted by the software in your host machine.
- C:\wamp\www\CamAnon\system\database\drivers\mysql\mysql_driver.php 91
